82b013f6c85646abc51429cb95c1beb3accf02f8
Mac-Eames <@Habeeb123>
2024-02-06 23:06:24 +0100

    initial commit

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -88,10 +88,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'restapidb',
-        'USER': 'postgres',
-        'PASSWORD': 'Habeeb123',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
